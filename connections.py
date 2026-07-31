bf3182f5bb38d1c5ec9ae78ca5cbed6e2575bc2d
sakwasilas <sakwahsilas@gmail.com>
2026-07-31 11:59:19 +0300

    added database url

--- connections.py
+++ connections.py
@@ -1,9 +1,25 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# path = "mysql+pymysql://root:2480@localhost/project_2"
+
+# engine = create_engine(path)
+
+# Session = sessionmaker(bind=engine)
+
+# db_session = Session()
+
+# Base = declarative_base()
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-path = "mysql+pymysql://root:2480@localhost/project_2"
+DATABASE_URL = (
+    "postgresql+psycopg2://lms_hqze_user:6pQyAYaNDW8rnYR5L44uv9XToQDWotpH"
+    "@dpg-d9m65fe7bikc73a5phm0-a.oregon-postgres.render.com:5432/lms_hqze"
+)
 
-engine = create_engine(path)
+engine = create_engine(DATABASE_URL)
 
 Session = sessionmaker(bind=engine)
 
