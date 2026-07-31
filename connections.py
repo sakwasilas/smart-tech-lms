ba876f396017f9dd6b77fe58c42bf2feed8d9f7c
sakwasilas <sakwahsilas@gmail.com>
2026-07-31 14:56:34 +0300

    new database

--- connections.py
+++ connections.py
@@ -1,10 +1,9 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = (
-    "postgresql+psycopg2://lms_hqze_user:6pQyAYaNDW8rnYR5L44uv9XToQDWotpH"
-    "@dpg-d9m65fe7bikc73a5phm0-a.oregon-postgres.render.com:5432/lms_hqze"
+    "postgresql+psycopg2://lms_383r_user:V2uGtsh9WtpZhU4yNxqeXtNs29n4chN7"
+    "@dpg-d9m8pd7qj5pc73abfbrg-a.oregon-postgres.render.com:5432/lms_383r"
 )
 
 engine = create_engine(DATABASE_URL)
