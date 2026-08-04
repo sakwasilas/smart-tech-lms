945222d155d3e9951881dbc72d625a08edd8c608
sakwasilas <sakwahsilas@gmail.com>
2026-08-04 12:09:32 +0300

    new system

--- connections.py
+++ connections.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
- DATABASE_URL = (
-     "postgresql+psycopg2://lms_383r_user:V2uGtsh9WtpZhU4yNxqeXtNs29n4chN7"
-     "@dpg-d9m8pd7qj5pc73abfbrg-a.oregon-postgres.render.com:5432/lms_383r"
- )
+ DATABASE_URL =(
+    "postgresql+psycopg2://lms_383r_user:V2uGtsh9WtpZhU4yNxqeXtNs29n4chN7"
+    "@dpg-d9m8pd7qj5pc73abfbrg-a.oregon-postgres.render.com:5432/lms_383r"
+)
 
 #DATABASE_URL = "mysql+pymysql://root:2480@localhost:3306/mywork"
 
