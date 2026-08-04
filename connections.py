cd53323706f79e79ebee162bddeb267f96d65dc1
sakwasilas <sakwahsilas@gmail.com>
2026-08-04 13:01:16 +0300

    Update PostgreSQL database connection

--- connections.py
+++ connections.py
@@ -2,11 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = (
-    "postgresql+psycopg2://lms_383r_user:V2uGtsh9WtpZhU4yNxqeXtNs29n4chN7"
-    "@dpg-d9m8pd7qj5pc73abfbrg-a.oregon-postgres.render.com:5432/lms_383r"
+    "postgresql+psycopg2://lms_397f_user:1zY0vMRHTsNR9Th2zBBfBrtfMTk7GCCb"
+    "@dpg-d9ordtrncjis73egne70-a.oregon-postgres.render.com:5432/lms_397f"
 )
-
-# DATABASE_URL = "mysql+pymysql://root:2480@localhost:3306/mywork"
 
 engine = create_engine(DATABASE_URL)
 
